cc7e30a4ef6c970b8dbe6967d69c36d982117263
decommandpro <93335212+decommandpro@users.noreply.github.com>
2022-12-20 17:59:16 +0100

    Update ACS.lua

--- ACS.lua
+++ ACS.lua
@@ -1,12 +1,34 @@
 local monitors = {peripheral.find("monitor")}
+local monitorTypes = {"Status", "LevelTrim", "RotTrim"}
+local definedMonitors = {}
 
 function DefineMonitors()
-    for i in pairs(monitors) do
-        i.setBackgroundColor(colors.gray)
-        i.clear()
-        i.setCursorPos(1, 1)
-        i.write("Define as Status Display")
+    for i, t in pairs(monitorTypes) do
+        for j, m in pairs(monitors) do
+            m.setBackgroundColor(colors.gray)
+            m.setTextColor(colors.white)
+            m.setTextScale(0.5)
+            m.clear()
+            m.setCursorPos(1, 1)
+            m.write("Define for:")
+            m.setCursorPos(1, 2)
+            m.write(t)
+        end
+        local _, side = os.pullEvent("monitor_touch")
+        definedMonitors[t] = peripheral.wrap(side)
+        definedMonitors[t].setBackgroundColor(colors.black)
+        definedMonitors[t].clear()
+        for j, m in pairs(monitors) do
+            print(m)
+            if m == definedMonitors[t] then
+                table.remove(monitors, j)
+                print("remove")
+            end
+        end
     end
 end
 
 DefineMonitors()
+for i, m in pairs(definedMonitors) do
+    m.write(i)
+end
